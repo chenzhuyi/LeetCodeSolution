990ab715b6583e9ca64f2f203b2dad417f8ec823
Claire Chen <claire.chen02@sap.com>
2022-01-14 15:27:49 +0800

    相交链表: a+b b+a

---
+++ src/leetcode/solution/IntersectionNode.java
@@ -0,0 +1,18 @@
+package leetcode.solution;
+
+public class IntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while(pa != pb) {
+            pa = pa == null? headB : pa.next;
+            pb = pb == null? headA : pb.next;
+        }
+        return pa;
+    }
+}
